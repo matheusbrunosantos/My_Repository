74c630e285bd398c7cc8e3592a1f285e3291fd83
Matheus <matheusbsnt@gmail.com>
2020-10-20 22:29:25 -0300

    OK

--- bd/equipamentos.php
+++ bd/equipamentos.php
@@ -1,15 +1,15 @@
 <?php
 	$equipamentos =[
-		['nome' => 'Matheus','categoria' => 'Eletrônicos','data_compra' => '20/10/2020','codigo' => '2626262'],
-		['nome' => 'Isabela','categoria' => 'Movéis','data_compra' => '1/10/2020','codigo' => '15116611'],
-		['nome' => 'Leonardo','categoria' => 'Eletrônicos','data_compra' => '13/10/2020','codigo' => '262626223'],
-		['nome' => 'Lucas','categoria' => 'Eletrônicos','data_compra' => '4/10/2020','codigo' => '374733636'],
-		['nome' => 'Daniel','categoria' => 'Eletrônicos','data_compra' => '13/10/2020','codigo' => '37843'],
-		['nome' => 'Leandro','categoria' => 'Movéis','data_compra' => '5/10/2020','codigo' => '64263'],
-		['nome' => 'Mozart','categoria' => 'Eletrônicos','data_compra' => '5/10/2020','codigo' => '363773'],
-		['nome' => 'Eduardo','categoria' => 'Movéis','data_compra' => '30/10/2020','codigo' => '997433'],
-		['nome' => 'Bruno','categoria' => 'Eletrônicos','data_compra' => '2/10/2020','codigo' => '784995'],
-		['nome' => 'Santos','categoria' => 'Movéis','data_compra' => '24/10/2020','codigo' => '2677844']
+		['nome' => 'Tela','data_compra' => '20/10/2020','codigo' => '2626262'],
+		['nome' => 'Telescopio','categoria' => 'Testes','codigo' => '15116611'],
+		['nome' => 'Telescopio 200w','categoria' => 'Eletrônicos','data_compra' => '13/10/2020','codigo' => '262626223'],
+		['nome' => 'RX L200','categoria' => 'Eletrônicos','data_compra' => '4/10/2020','codigo' => '374733636'],
+		['nome' => 'RX 200-18','categoria' => 'Eletrônicos','data_compra' => '13/10/2020','codigo' => '37843'],
+		['nome' => 'Impressora','categoria' => 'Testes','data_compra' => '5/10/2020','codigo' => '64263'],
+		['nome' => 'Tubo de ensaio','categoria' => 'Eletrônicos','data_compra' => '5/10/2020','codigo' => '363773'],
+		['nome' => 'Centrifuga','categoria' => 'Testes','data_compra' => '30/10/2020','codigo' => '997433'],
+		['nome' => 'Estufa','categoria' => 'Eletrônicos','data_compra' => '2/10/2020','codigo' => '784995'],
+		['nome' => 'Autoclave','categoria' => 'Testes','data_compra' => '24/10/2020','codigo' => '2677844']
 
 	];
 ?>
--- equipamentos.php
+++ equipamentos.php
@@ -5,7 +5,9 @@ include_once('layout/sidebar.php');
 include_once('bd/equipamentos.php');
 ?>
 <div class="col">
-  <h2>Equipamentos</h2>
+  <h2 class="titulo">Equipamentos</h2>
+  <span class="badge badge-info totais">Total: <?php echo count($equipamentos); ?></span>
+  <div class="clear"></div>
   <div class="card">
     <div class="card-body">
       <a href="form_equipamentos.php" class="btn btn-primary" style="float: right">
@@ -24,15 +26,14 @@ include_once('bd/equipamentos.php');
             <th>Código</th>
             <th class="acao">Ação</th>
           </tr>
-
           <?php foreach ($equipamentos as $key => $equipamentos): ?>
           <tr>
             <td><?= $equipamentos['nome'] ?></td>
-            <td><?= $equipamentos['categoria'] ?></td>
-            <td><?= $equipamentos['data_compra'] ?></td>
+            <td><?= (isset($equipamentos['categoria']) ? $equipamentos['categoria'] : 'Não definida') ?></td>
+            <td><?= $equipamentos['data_compra'] ?? 'Não informada' ?></td>
             <td><?= $equipamentos['codigo'] ?></td>
             <td>
-              <a href="#" class="btn btn-success">
+              <a href="#" class="btn btn-secondary">
                 <i class="fas fa-eye"></i>
               </a>
               <a href="#" class="btn btn-warning">
